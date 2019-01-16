d5c0d5c2db8cf94444b54f7091d1d3134d622baa
stefa168 <stefa168@hotmail.it>
2019-01-16 12:58:53 +0100

    Spostato il metodo di debug delle impostazioni.

--- gestore.c
+++ gestore.c
@@ -14,11 +14,43 @@
 int main(int argc, char *argv[]) {
     SettingsData *settings;
     int msgID;
-    int i;
 
     settings = readConfiguration(argc, argv);
 
 #ifdef DEBUG_GESTORE
+    printFoundSettings(settings);
+#endif
+
+    // Gestire prima della logica degli studenti tutta la parte della memoria e il comportamento del gestore
+
+
+
+    // IPC CREAT e EXCL insieme creano la coda se non esiste, ma se esiste lanciano un errore.
+//    msgID = msgget(IPC_SIM_KEY, IPC_CREAT | IPC_EXCL | S_IWUSR | S_IRUSR);
+//
+//    if (msgID == -1) {
+//        PRINT_ERROR("Errore durante l'apertura della coda di messaggi\n")
+//        PRINT_ERRNO_EXIT(-1)
+//    } else {
+//        printf("Coda di messaggi aperta correttamente con ID %d\n", msgID);
+//    }
+//
+//    // msgctl serve anche per cancellare la coda.
+//    // consigliato ipcs per vedere se ci sono delle cose che non vogliamo
+//    // consigliato ipcrm --all=msg per rimuovere tutti i messaggi
+//    if (msgctl(msgID, IPC_RMID, NULL)) {
+//        fprintf(stderr, "%s:%d: Errore durante la chiusura della coda di messaggi %d\n", __FILE__, __LINE__, msgID);
+//        PRINT_ERRNO_EXIT(-1)
+//    } else {
+//        printf("Coda di messaggi %d chiusa correttamente.\n", msgID);
+//    }
+
+    return 0;
+}
+
+void printFoundSettings(SettingsData *settings) {
+    int i;
+
     printf("Trovate le impostazioni:\n"
            "\t- pop_size = %d\n"
            "\t- sim_duration = %d\n"
@@ -41,29 +73,4 @@ int main(int argc, char *argv[]) {
             printf("%d,", settings->preferencePercentages[i]);
         }
     }
-#endif
-
-    // Gestire prima della logica degli studenti tutta la parte della memoria e il comportamento del gestore
-
-    // IPC CREAT e EXCL insieme creano la coda se non esiste, ma se esiste lanciano un errore.
-    msgID = msgget(IPC_OWN_KEY, IPC_CREAT | IPC_EXCL | S_IWUSR | S_IRUSR);
-
-    if (msgID == -1) {
-        PRINT_ERROR("Errore durante l'apertura della coda di messaggi\n")
-        PRINT_ERRNO_EXIT(-1)
-    } else {
-        printf("Coda di messaggi aperta correttamente con ID %d\n", msgID);
-    }
-
-    // msgctl serve anche per cancellare la coda.
-    // consigliato ipcs per vedere se ci sono delle cose che non vogliamo
-    // consigliato ipcrm --all=msg per rimuovere tutti i messaggi
-    if (msgctl(msgID, IPC_RMID, NULL)) {
-        fprintf(stderr, "%s:%d: Errore durante la chiusura della coda di messaggi %d\n", __FILE__, __LINE__, msgID);
-        PRINT_ERRNO_EXIT(-1)
-    } else {
-        printf("Coda di messaggi %d chiusa correttamente.\n", msgID);
-    }
-
-    return 0;
 }
--- gestore.h
+++ gestore.h
@@ -1,4 +1,8 @@
 #ifndef ESERCIZI_SO_GESTORE_H
 #define ESERCIZI_SO_GESTORE_H
 
+#include "utilities/settings_reader.h"
+
+void printFoundSettings(SettingsData *settings);
+
 #endif
